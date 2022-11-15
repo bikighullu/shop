16d843ecc9782bb17e5ecabf077686d36dcbe772
biki ghullu <biki.ghulu@gmail.com>
2022-11-15 15:24:27 +1030

    get data from server and dispatching action

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import ProductComponent from "./ProductComponents";
+import { useSelector, useDispatch } from "react-redux";
+import { setProducts } from "../redux/actions/productActions";
 
 const ProductListing = () => {
+  const products = useSelector((state) => state);
+
+  const dispatch = useDispatch();
+  const fetchProducts = async () => {
+    const response = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((err) => {
+        console.log("Err", err);
+      });
+
+    dispatch(setProducts(response.data));
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log("Products: ", products);
   return (
-    <div>
-      <h1> Product Listing</h1>
+    <div className="ui grid container">
+      <ProductComponent />
     </div>
   );
 };

e0e5048f23ccaf32955123d6fdc8776747a140e0
biki ghullu <biki.ghulu@gmail.com>
2022-11-16 13:45:02 +1030

    shop complete

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -1,24 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const ProductComponent = () => {
   const products = useSelector((state) => state.allProducts.products);
+
   const renderList = products.map((product) => {
     const { id, title, image, price, category } = product;
     return (
-      <div className="four column wide" key={id}>
-        <div className="ui link cards">
-          <div className="card">
-            <div className="image">
-              <img src={image} alt={title} />
-            </div>
-            <div className="content">
-              <div className="header">{title}</div>
-              <div className="meta price">${price}</div>
-              <div className="meta">{category}</div>
+      <div className="ui grid four wide column" key={id}>
+        <Link to={`/product/${id}`}>
+          <div className="ui link cards">
+            <div className="card">
+              <div className="ui small images">
+                <img src={image} alt={title} />
+              </div>
+              <div className="content">
+                <div className="header">{title}</div>
+                <div className="meta price">${price}</div>
+                <div className="meta">{category}</div>
+              </div>
             </div>
           </div>
-        </div>
+        </Link>
       </div>
     );
   });
